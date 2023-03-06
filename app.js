d1ceb8b8e6449ea64ba1c10506889b2aad226c8b
gabrielvdb <108374613+gabrielvdb@users.noreply.github.com>
2023-03-06 12:57:06 -0300

    terminado

--- app.js
+++ app.js
@@ -13,3 +13,11 @@ app.get("/productCart", function(req,res){
 app.listen(port, function(){
     console.log("Servidor escuchando en le puerto 3000 http://localhost:" + port);
 });  
+
+
+app.use(express.static(path.resolve(__dirname,"../public")));
+
+
+app.get("/home.html",function(req,res){
+    res.sendFile(path.join(__dirname,"src/views/home.html"));
+})
